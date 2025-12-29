67cbb1c034a42ffe90ea534b2de6967e544b8893
aShalimd <146211372+aShalimd@users.noreply.github.com>
2025-12-29 14:01:40 +0530

    Add files via upload

---
+++ Problem4.java
@@ -0,0 +1,27 @@
+package Tandemloop;
+
+public class Problem4 {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30};
+        int[] count = new int[10];
+
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[j] % i == 0) {
+                    count[i]++;
+                }
+            }
+        }
+
+        System.out.print("{");
+        for (int i = 1; i <= 9; i++) {
+            System.out.print(i + ": " + count[i]);
+            if (i < 9) {
+                System.out.print(", ");
+            }
+        }
+        System.out.print("}");
+    }
+}
